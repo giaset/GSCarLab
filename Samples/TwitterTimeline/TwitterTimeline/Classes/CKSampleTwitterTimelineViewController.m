8397e575177dbff2d29df2dcb2bb3542cb2e4062
Gianni Settino <gianni@wherecloud.com>
2013-12-20 17:27:24 -0500

    Now works properly on iOS5. Only supports iOS 7.0. Cells now select properly

--- Samples/TwitterTimeline/TwitterTimeline/Classes/CKSampleTwitterTimelineViewController.m
+++ Samples/TwitterTimeline/TwitterTimeline/Classes/CKSampleTwitterTimelineViewController.m
@@ -66,12 +66,39 @@
         cell.imageView.frame = CGRectMake(controller.contentInsets.left,controller.contentInsets.top,44,44);
     }];
     
-    //Set setup block
     [cellController setSetupBlock:^(CKTableViewCellController *controller, UITableViewCell *cell) {
         CKSampleTwitterTweetModel *thisTweet = (CKSampleTwitterTweetModel *) controller.value;
         
-        CKImageView *img = (CKImageView *) cell.imageView;
-        img.imageURL = thisTweet.imageUrl;
+        UIToolbar *toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0,cell.frame.size.height-44,320,44)];
+        toolbar.tintColor = [UIColor whiteColor];
+        toolbar.barTintColor = [UIColor redColor];
+        
+        UIBarButtonItem *previousButton = [[UIBarButtonItem alloc]initWithTitle:_(@"Previous") style:UIBarButtonItemStyleBordered block:^{
+        }];
+        
+        UIBarButtonItem *nextButton = [[UIBarButtonItem alloc]initWithTitle:_(@"Next") style:UIBarButtonItemStyleBordered block:^{
+        }];
+        
+        UIBarButtonItem *flexButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone block:^{
+        }];
+        
+        NSArray *itemsArray = [NSArray arrayWithObjects:previousButton, nextButton, flexButton, doneButton, nil];
+        
+        [toolbar setItems:itemsArray];
+        
+        [cell.contentView addSubview:toolbar];
+        
+        [cell beginBindingsContextByRemovingPreviousBindings];
+        
+        [bself bind:@"selectedTweet" executeBlockImmediatly:YES withBlock:^(id value) {
+            cell.backgroundColor = (bself.selectedTweet == thisTweet) ? [UIColor colorWithRGBValue:0x333333] : [UIColor whiteColor];
+            cell.textLabel.textColor = (bself.selectedTweet == thisTweet) ? [UIColor whiteColor] : [UIColor blackColor];
+            cell.detailTextLabel.textColor = (bself.selectedTweet == thisTweet) ? [UIColor whiteColor] : [UIColor blackColor];
+            toolbar.hidden = !(bself.selectedTweet == thisTweet);
+        }];
+        
+        [cell endBindingsContext];
     }];
     
     return cellController;
